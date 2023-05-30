3fd176c7ff9311555f6068d1a102337f8883bc56
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-05-30 19:31:13 +0530

    refactor : get current hour , minutes and sec using date object

--- Day2/brain.js
+++ Day2/brain.js
@@ -1,3 +1,10 @@
 const hourhand = document.querySelector(".hourhand");
 const minhand = document.querySelector(".minhand");
 const sechand = document.querySelector(".sechand");
+setInterval( setdate, 1000)
+function setdate() {
+    const date= new Date();
+    const hourhandtime=date.getHours();
+    const minhandtime=date.getMinutes();
+    const sechandtime=date.getSeconds();
+}
