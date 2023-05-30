ded63761b8fcc9d762cf470259ac6f7a661ee600
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-05-30 19:35:46 +0530

    set no-transition class on sechand hourhand and minhand
    At these place we check if current rotation of hours ,min and sec is equal ro 0 or not . If it is equat to 0 then add no-transition class on corresponding hands otherwise remove no-transtion
    class from corressponding hands

--- Day2/brain.js
+++ Day2/brain.js
@@ -10,4 +10,22 @@ function setdate() {
     const hourhandrotate = ((hourhandtime / 12) * 360)+ (minhandtime/2);
     const minhandrotate = ((minhandtime / 60) * 360) +(sechandtime / 6); ;
     const sechandrotate=((sechandtime / 60) * 360);
+    if (hourhandrotate==0) {
+        hourhand.classList.add("no-transition");
+    }
+    else{
+        hourhand.classList.remove("no-transition");
+    }
+    if (minhandrotate==0) {
+        minhand.classList.add("no-transition");
+    }
+    else{
+        minhand.classList.remove("no-transition");
+    }
+    if (sechandrotate==0) {
+        sechand.classList.add("no-transition");
+    }
+    else{
+        sechand.classList.remove("no-transition");
+    }
 }
