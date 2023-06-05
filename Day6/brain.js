edd450127630765554d1fd6336279da7b10fcb5f
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-06-05 23:47:20 +0530

    In displayMatches display the filtered cities

--- Day6/brain.js
+++ Day6/brain.js
@@ -9,3 +9,17 @@ function findMatches(wordtomatch, cities) {
         return places.city.match(regex) || places.state.match(regex);
     });
 }
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+    const list_html = matchArray.map(places => {
+        const regex = new RegExp(this.value);
+        const city_name=places.city.replace(regex,`<span class="highlight">${this.value}</span>`);
+        const state_name=places.city.replace(regex,`<span class="highlight">${this.value}</span>`);
+        return `
+    <li>
+    <span class="name">${city_name},${state_name}</span>
+    <span class="population">${places.population}</span>
+    </li>
+    `}).join(" ");
+    suggestionbox.innerHTML=list_html;
+}
