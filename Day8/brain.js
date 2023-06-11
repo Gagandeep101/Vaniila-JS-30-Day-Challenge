65c12530b1f77e0f12e662b5561b5510dbbb0bfe
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-06-11 18:01:22 +0530

    declare draw function
    in draw function we set linestyle and match position with mouse and changing its width and color on mousing mouse

--- Day8/brain.js
+++ Day8/brain.js
@@ -10,6 +10,31 @@ let lastX = 0;
 let lastY = 0;
 let hue=0;
 let direction = true;
+function draw(e) {
+    if (!isdrawing) {
+        return
+    }
+    ctx.beginPath();
+    ctx.strokeStyle = `hsl(${hue}, 90%, 50%)`;
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+    hue++;
+    if (hue===359) {
+        hue=0;
+    }
+    if (ctx.lineWidth >=100 || ctx.lineWidth <=1) {
+        direction = !direction;
+    }
+    if (direction) {
+        ctx.lineWidth++;
+    }
+    else{
+        ctx.lineWidth--;
+    }
+}
 
 // By Using four event listener we see position at which mouse is clicked to that position when the mouse is over
 canvas.addEventListener("mousemove", draw);
