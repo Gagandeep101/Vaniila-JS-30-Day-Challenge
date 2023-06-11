b0d301228e081e02b03121addfe9e4fa1c5c747f
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-06-11 18:28:05 +0530

    Add eventlistener to all checkboxes

--- Day10/brain.js
+++ Day10/brain.js
@@ -1,2 +1,4 @@
 const checkboxes = document.querySelectorAll('.item input[type="checkbox"]');
 let lastChecked;
+
+checkboxes.forEach(checkbox => { checkbox.addEventListener("click", handleCheck); });
