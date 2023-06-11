84bcd0fdcf226334893a64d5be3b231f7f3a02f2
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-06-11 17:59:38 +0530

    Add eventlistener on canvas
    We add for event listener on canvas first move move to draw line using draw function . MouseDown set lastX and LastY to current mouse position  and makng is drawing true. Atlast add mouseup and mouseout to make isdrawing false for mouse is reala=sing no more drawing on canvas

--- Day8/brain.js
+++ Day8/brain.js
@@ -10,3 +10,13 @@ let lastX = 0;
 let lastY = 0;
 let hue=0;
 let direction = true;
+
+// By Using four event listener we see position at which mouse is clicked to that position when the mouse is over
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", (e) => {
+    isdrawing = true
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+});
+canvas.addEventListener("mouseup", () => isdrawing = false);
+canvas.addEventListener("mouseout", () => isdrawing = false);
