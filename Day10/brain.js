505474ee5098332e649ad63e9174ab845bef0a6d
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-06-11 18:31:02 +0530

    declare handleCheck function
    inside function create var inbetween with false and lastcheck to current checkbox

--- Day10/brain.js
+++ Day10/brain.js
@@ -1,4 +1,7 @@
 const checkboxes = document.querySelectorAll('.item input[type="checkbox"]');
 let lastChecked;
-
+function handleCheck() {
+    let inBetween = false;
+    lastChecked = this;
+}
 checkboxes.forEach(checkbox => { checkbox.addEventListener("click", handleCheck); });
