461620fa92f015190b22d96a9aba0c93e8dc69b1
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-06-13 19:49:54 +0530

    eventlisterner on window and declare check_slider function

--- Day12/brain.js
+++ Day12/brain.js
@@ -13,3 +13,25 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
     };
 };
+function check_slider() {
+    slide_in.forEach(slideImage => {
+
+        // Go half way of image height through scroll the window
+        
+        const slide_in_at = (window.scrollY + window.innerHeight) - slideImage.height / 2;
+        
+        // Go to bottom of image height through scroll the window
+
+        const slide_bottom = slideImage.offsetTop + slideImage.height;
+        const isHalfShown = slide_in_at > slideImage.offsetTop;
+        const isnotScrolledMid = window.scrollY < slide_bottom;
+        if (isHalfShown && isnotScrolledMid) {
+            slideImage.classList.add('active');
+        } else {
+            slideImage.classList.remove('active');
+    
+        }
+    });
+}
+const slide_in = document.querySelectorAll('.slide-in');
+window.addEventListener('scroll', debounce(check_slider));
