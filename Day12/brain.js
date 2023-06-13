556e15cfbca4c32f9aac30e8935d1ab51526ac96
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-06-13 19:43:31 +0530

    declare debounce function

---
+++ Day12/brain.js
@@ -0,0 +1,15 @@
+// In debounce function wait for 20 time scroll events before make visible image visible
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
