c8e5e08220eab8fb4c0c95c87b758c73c966b107
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-06-16 19:11:06 +0530

    Render the item in itemList

--- Day14/brain.js
+++ Day14/brain.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
 const items = [];
-
 
 function addItem(e) {
   e.preventDefault();
@@ -10,7 +10,19 @@ function addItem(e) {
     done: false
   };
   items.push(item);
+  populateList(items, itemsList);
   this.reset();
+}
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+    <li>
+    <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''}/>
+    <label for="item${i}">${plate.text}</label>
+    </li>
+    `;
+  }).join('');
 }
 
 addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
