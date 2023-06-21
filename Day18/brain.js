7013f0cc4e9c303f1d1c90bea58e9185bc907fbb
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-06-21 18:39:57 +0530

    use recognitation and append into words class div tag

--- Day18/brain.js
+++ Day18/brain.js
@@ -1,2 +1,18 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
+// Use speechRecognition object and change its property interimResults and language
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+// Use paragaraph element and append in words div tags
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+// Add event listeners on recognition after completing speech show results
+recognition.addEventListener('result', e => {
+  console.log(e);
+});
+recognition.start();
