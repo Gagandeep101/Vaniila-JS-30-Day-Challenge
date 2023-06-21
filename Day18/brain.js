ee12cc4efdae6ee77d4686e3c3f03d84cd94e02a
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-06-21 18:58:22 +0530

    Refactor the speech recognition code

--- Day18/brain.js
+++ Day18/brain.js
@@ -13,6 +13,18 @@ words.appendChild(p);
 
 // Add event listeners on recognition after completing speech show results
 recognition.addEventListener('result', e => {
-  console.log(e);
+
+  // Map the event into results go inside into speechRecognitionResults to transcript and then join all of them with space
+  const transcript= Array.from(e.results).map(results => results[0]).map(result => result.transcript).join(' ');
+
+  // Check if e.results[0].isFinal is true then create a new element of paragraph and then append into words
+  if(e.results[0].isFinal){
+    p= document.createElement('p');
+    p.textContent = transcript;
+    words.appendChild(p);
+  }
 });
+
+// When recognition are stopped we restart again using recognition.start
+recognition.addEventListener('end',recognition.start);
 recognition.start();
