0a6f23812b6a289f4ce17ca9d316a30173f65cb5
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-06-24 17:10:10 +0530

    fetch basic coordinate in js

--- Day20/brain.js
+++ Day20/brain.js
@@ -2,3 +2,9 @@ const trigger =document.querySelectorAll('a');
 const highlighted = document.createElement('span');
 highlighted.classList.add('highlight');
 document.body.append(highlighted);
+
+function highlightlink() {
+    const linkCoord=this.getBoundingClientRect();
+    console.log(linkCoord);
+}
+trigger.forEach(a => a.addEventListener('mouseenter',highlightlink));
