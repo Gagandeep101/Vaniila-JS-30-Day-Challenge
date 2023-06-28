6da881aabae4e67fb8fcc77d9f0ddc4fa17f244a
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-06-28 19:11:06 +0530

    SpeechSynthesisUtterance text property set

--- Day21/brain.js
+++ Day21/brain.js
@@ -4,3 +4,8 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+// set message SpeechSynthesisUtterance text propety with textarea text 
+msg.text = document.querySelector('[name="text"]').value;
+
+console.log(msg);
