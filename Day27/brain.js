a92e8d07d6047759c0cedab27b4000772a334eac
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-07-02 15:21:11 +0530

    declare timer function

--- Day27/brain.js
+++ Day27/brain.js
@@ -2,3 +2,20 @@ let countdown;
 const displayTimer = document.querySelector('.display__time-left');
 const displayEndTimer = document.querySelector('.display__end-time');
 const button = document.querySelectorAll('[data-time]');
+
+// In these function get seconds and update the counter on every seconds
+function timer(seconds) {
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    countdown = setInterval(() => {
+        const secondleft = Math.round((then - Date.now()) / 1000);
+        
+        // if second left is less than 0 then stop updating counter
+        if (secondleft <0) {
+            clearInterval(countdown);
+            return;
+        }
+        console.log(secondleft);
+    }, 1000);
+}
+timer(10);
