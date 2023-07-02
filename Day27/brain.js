73fc654e1a89be747c751c3a98d39fd3d33c7f69
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-07-02 15:46:57 +0530

    display Time when timer stop

--- Day27/brain.js
+++ Day27/brain.js
@@ -10,11 +10,13 @@ function timer(seconds) {
 
     // display_timer used to sound set countdown timer before updating it
     display_timer(seconds);
+
+    display_end_timer(then - now);
     countdown = setInterval(() => {
         const secondleft = Math.round((then - Date.now()) / 1000);
-        
+
         // if second left is less than 0 then stop updating counter
-        if (secondleft <0) {
+        if (secondleft < 0) {
             clearInterval(countdown);
             return;
         }
@@ -23,11 +25,19 @@ function timer(seconds) {
 }
 
 function display_timer(seconds) {
-    const min=Math.round(seconds / 60);
-    const sec=seconds % 60;
-    console.log(min,sec);
-    displayTimer.textContent=`${show_time_correct(min)}:${show_time_correct(sec)}`;
+    const min = Math.round(seconds / 60);
+    const sec = seconds % 60;
+    displayTimer.textContent = `${show_time_correct(min)}:${show_time_correct(sec)}`;
+}
+
+function display_end_timer(seconds) {
+    const end_date = new Date(seconds);
+    const hour = end_date.getHours();
+    const min = end_date.getMinutes();
+    const sec = end_date.getSeconds();
+    displayEndTimer.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour} : ${show_time_correct(min)} : ${show_time_correct(sec)}`;
 }
 function show_time_correct(time) {
-    return time<10? "0"+time:time;
+    return time < 10 ? "0" + time : time;
 }
+timer(10);
