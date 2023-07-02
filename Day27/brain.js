6980490d94c430b8bbfd5ec2b31e39b0945e513d
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-07-02 15:19:34 +0530

    import all html element needed in to implement countdown timer

---
+++ Day27/brain.js
@@ -0,0 +1,4 @@
+let countdown;
+const displayTimer = document.querySelector('.display__time-left');
+const displayEndTimer = document.querySelector('.display__end-time');
+const button = document.querySelectorAll('[data-time]');
