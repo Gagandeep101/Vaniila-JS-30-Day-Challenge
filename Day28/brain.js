ff881ff3b54fd9bdaa39ed95df3481b410518821
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-07-04 00:15:51 +0530

    update the video time show bar on playing video

--- Day28/brain.js
+++ Day28/brain.js
@@ -29,11 +29,16 @@ function handleRangeUpdate() {
     video[this.name] = this.value;
 }
 
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
 
 // Hook up the event listeners;
 video.addEventListener('click', togglePlay);
 video.addEventListener('pause', updatePlayButton);
 video.addEventListener('play', updatePlayButton);
+video.addEventListener('timeupdate', handleProgress);
 skipButtons.forEach(button => {
     button.addEventListener('click', skip);
 });
