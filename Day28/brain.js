d6fbf50a12e15a1f529bcc0733ace67e820769c3
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-07-04 00:17:30 +0530

    remove bugs when user want to play and pause from icon

--- Day28/brain.js
+++ Day28/brain.js
@@ -39,6 +39,7 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('pause', updatePlayButton);
 video.addEventListener('play', updatePlayButton);
 video.addEventListener('timeupdate', handleProgress);
+toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => {
     button.addEventListener('click', skip);
 });
