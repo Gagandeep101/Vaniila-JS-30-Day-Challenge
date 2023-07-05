a526c453bf69d1318af607e2a50971ee44ba7cb1
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-07-05 19:44:13 +0530

    select the random hole from holes

--- Day30/brain.js
+++ Day30/brain.js
@@ -1,7 +1,19 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lasthole=false;
 
 function randTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
+}
+function pickRandomHole(holes) {
+    // select random hole with random idx
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    // if current hole is same as last hole then auto call these function again and again
+    if (hole === lasthole) {
+        return pickRandomHole(holes);
+    }
+    lasthole = hole;
+    return hole;
 }
