59c986931a16c57daec34306e36df5e43fa0fad3
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-07-05 18:05:28 +0530

    show webcam video on screen

--- Day29/brain.js
+++ Day29/brain.js
@@ -3,3 +3,13 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+// get webcam access from useer and display a webcom video on top of the the screen
+
+function getVideo(){
+  // get onely video from user 
+  navigator.mediaDevices.getUserMedia({video: true,audio: false}).then(localMediaStream=>{
+    video.srcObject = localMediaStream;
+    video.play();
+  }).catch(err=>console.log("Please wait... || Check your WebCams settings",err.msg));
+}
+getVideo();
